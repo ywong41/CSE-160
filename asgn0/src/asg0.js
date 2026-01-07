b0587b9a0872aa06aa27dbcf0412f2a9040886b5
root <root@DESKTOP-5IFO51V.localdomain>
2026-01-07 14:06:18 -0800

    step 3, added handleDrawEvent(), interface, and button

--- asgn0/src/asg0.js
+++ asgn0/src/asg0.js
@@ -22,14 +22,14 @@ function main() {
     */
 
     // Step 2
-    ctx.fillStyle = 'black'; // set canvas to black
-    ctx.fillRect(0, 0, 400, 400);
+    ctx.fillStyle = 'black'; // set fill color to black
+    ctx.fillRect(0, 0, 400, 400); // draws a filled rectangles
 
     var v1 = new Vector3([2.25, 2.25, 0]); // set the z coordinate to zero
     drawVector(v1, "red"); // draw vector v1 and a string color in red
 }
 
-// a function drawVector(v, color) that takes a Vector3 v and a string color
+// a function named drawVector(v, color) that takes a Vector3 v and a string color
 function drawVector(v, color){
     // center of 400x400 canvas
     var center_x = 400/2;
@@ -46,4 +46,20 @@ function drawVector(v, color){
     ctx.moveTo(center_x, center_y);
     ctx.lineTo(center_x + v.elements[0] * scale, center_y - v.elements[1] * scale); // scale vector by 20 (canvas y grows downward)
     ctx.stroke();
+}
+// a function named handleDrawEvent() that is called whenever a user clicks on the draw button
+function handleDrawEvent(){
+    // clear canvas
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    ctx.fillStyle = 'black'; // set fill color to black
+    ctx.fillRect(0, 0, 400, 400); // draws a filled rectangles
+    
+    // read text boxes values
+    var v1x = parseFloat(document.getElementById('v1_xCoor').value);
+    var v1y = parseFloat(document.getElementById('v1_yCoor').value);
+
+    // create v1
+    var v1 = new Vector3([v1x, v1y, 0]);
+    drawVector(v1, "red"); // draw vector v1 and a string color in red
 }
