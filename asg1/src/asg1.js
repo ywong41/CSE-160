05a06dc9e686077889e465bde3787b4fcd4fe0ba
root <root@DESKTOP-5IFO51V.localdomain>
2026-01-16 01:39:03 -0800

    webgl setup

--- asg1/src/asg1.js
+++ asg1/src/asg1.js
@@ -1,40 +1,24 @@
-// HelloPoint1.js (c) 2012 matsuda
-// Vertex shader program
-var VSHADER_SOURCE = 
-  'void main() {\n' +
-  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
-  '  gl_PointSize = 10.0;\n' +                    // Set the point size
-  '}\n';
+// DrawTriangle.js (c) 2012 matsuda
 
-// Fragment shader program
-var FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
-  '}\n';
+// make ctx and canvas Global var
+let canvas
+let gl
 
-function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+function main() {  
+    // webGL setup
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');  
+    if (!canvas) { 
+        console.log('Failed to retrieve the <canvas> element');
+        return false; 
+    } 
+    // a variable for drawing all the component on canvas
+    gl = getWebGLContext(canvas);
+    if(!gl){
+        console.log('Failed to retrieve WebGL content');
+        return false;
+    }
+    console.log(gl);
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
 
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
 }
