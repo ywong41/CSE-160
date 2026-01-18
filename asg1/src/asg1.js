8554e21b37f5402430689d39138ae822a7e42ce5
root <root@DESKTOP-5IFO51V.localdomain>
2026-01-18 15:18:21 -0800

    added click() and connectCoordinatesEventToGL()

--- asg1/src/asg1.js
+++ asg1/src/asg1.js
@@ -1,10 +1,100 @@
-// DrawTriangle.js (c) 2012 matsuda
-
-// make ctx and canvas Global var
+// global variables
 let canvas
 let gl
+let a_Position
+let u_FragColor
+
+// Vertex shader program
+var VERTEX_SHADER = `
+    precision mediump float;
+    attribute vec4 a_Position;
+    void main() {
+        gl_Position = a_Position; 
+        gl_PointSize = 10.0;
+    }`
+
+// Fragment shader program
+var FEAGMENT_SHADER = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
+    void main() {
+        gl_FragColor = u_FragColor;
+    }`
 
 function main() {  
+ 
+  
+    // Step 3
+    // set up canvas and gl
+    setupWebGL();
+    // set up GLSL shader program and connect to GLSL variables
+    connectVariablesToGLSL();
+
+    // Register function (event handler) to be called on a mouse press
+    canvas.onmousedown = click;
+
+    // specify the clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // rbg and alpha color(transparency)
+
+    // apply clear and color to canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    console.log(gl);
+}
+
+
+var g_points = [];  // The array for the position of a mouse press
+var g_colors = [];  // The array to store the color of a point
+function click(ev) {
+
+    // Extract the event click and return it in WebGL coordinates
+    [x,y] = connectCoordinatesEventToGL(ev);
+    
+    // Store the coordinates to g_points array
+    g_points.push([x, y]);
+
+    // Store the coordinates to g_points array
+    if (x >= 0.0 && y >= 0.0) {      // First quadrant
+        g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+    } else if (x < 0.0 && y < 0.0) { // Third quadrant
+        g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+    } else {                         // Others
+        g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+    }
+    renderAllShapes();
+}
+
+function connectCoordinatesEventToGL(ev){
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+
+    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+    return([x,y]);
+}
+
+// Draw every shape that suppose to be in canvas
+function renderAllShapes(){
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_points.length;
+    for(var i = 0; i < len; i++) {
+        var xy = g_points[i];
+        var rgba = g_colors[i];
+
+        // Pass the position of a point to a_Position variable
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        // Draw
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
+
+// get the canvas and gl context
+function setupWebGL(){
     // webGL setup
     // Retrieve <canvas> element
     canvas = document.getElementById('webgl');  
@@ -18,13 +108,28 @@ function main() {
         console.log('Failed to retrieve WebGL content');
         return false;
     }
-
-    // assign the clear color
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // rbg and alpha color(transparency)
-    // apply clear and color to canvas
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    console.log(gl);
-
- 
-
 }
+
+// compile the shader programs, attach the javascript variables to the GLSL variables
+function connectVariablesToGLSL(){
+    // initialize shaders
+    if(!initShaders(gl, VERTEX_SHADER, FEAGMENT_SHADER)){
+        console.log('Failed to initialize shaders');
+        return false;
+    }
+
+    // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return false;
+    }
+
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return false;
+    }
+}
+
