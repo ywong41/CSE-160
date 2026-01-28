80aff0025aadfb29f28867bc83336caa565d9db3
root <root@DESKTOP-5IFO51V.localdomain>
2026-01-27 17:09:39 -0800

    remove asgn1 stuff

--- asg2/src/asg2.js
+++ asg2/src/asg2.js
@@ -20,29 +20,28 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
-let u_Size;
 let u_ModelMatrix;
 let u_GlobalRotateMatrix;
 
-// Global variable for picture
-let g_pictureShapes = [];
-
-
-// constants
-const POINT = 0;
-const TRIANGLE = 1;
-const CIRCLE  = 2;
 
 // Global variable for UI
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
-let g_selectedSize = 5;
-let g_selectedType = POINT;
-let g_cirSegment = 10;  // default number of circle segment
 let g_yellowAngle = 0;
 let g_magentaAngle = 0;
-let g_globalAngle = 0;
 let g_yellowAnimation = false;
 let g_magentaAnimation = false;
+
+// Crocodile 
+let g_globalAngle = 0;
+let g_tailAngle = 0;
+let g_jawAngle = 0;
+let g_tailAnimation = false;
+let g_jawAnimation = false;
+
+// 3-level joint
+let g_thigh = 0;
+let g_calf  = 0;
+let g_foot  = 0;
 
 window.onload = function() {
     main();
@@ -50,14 +49,7 @@ window.onload = function() {
 
 // Set up action for the HTML UI elements
 function addActionsForHtmlUI(){
-    // button events (Shape type)
-    // document.getElementById('green').onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
-    // document.getElementById('red').onclick = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
-    document.getElementById('clearButton').onclick = function() { g_shapesList=[]; renderScene(); };   // clear that list to clear out all the point
-    document.getElementById('pointButton').onclick = function() { g_selectedType=POINT};   // clear that list to clear out all the point
-    document.getElementById('triButton').onclick = function() { g_selectedType=TRIANGLE};   // clear that list to clear out all the point
-    document.getElementById('circleButton').onclick = function() { g_selectedType=CIRCLE};   // clear that list to clear out all the point
-
+   
     // Button Events
     document.getElementById('animationYellowOnButton').onclick = function() { g_yellowAnimation = true; };
     document.getElementById('animationYellowOffButton').onclick = function() { g_yellowAnimation = false; };
@@ -66,19 +58,11 @@ function addActionsForHtmlUI(){
     document.getElementById('animationMagentaOnButton').onclick = function() { g_magentaAnimation = true; };
     document.getElementById('animationMagentaOffButton').onclick = function() { g_magentaAnimation = false; };
 
-    // Slider events
-    document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
-    document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
-    document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
-    document.getElementById('cirSegment').addEventListener('input', function() { g_cirSegment = parseInt(this.value); });   // a slider to control number of segments in circle 
-    // Size Slider Events
-    document.getElementById('sizeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
     // Color Slider Events
     document.getElementById('yellowSlide').addEventListener('mousemove', function() { g_yellowAngle = this.value; renderScene(); });
     document.getElementById('magentaSlide').addEventListener('mousemove', function() { g_magentaAngle = this.value; renderScene(); });
 
     document.getElementById('angleSlide').addEventListener('mousemove', function() { g_globalAngle = this.value; renderScene(); });
-    document.getElementById('drawPicture').onclick = drawPicture;
 
 }
 
@@ -185,7 +169,6 @@ function renderScene(){
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // var len = g_points.length;
     var len = g_shapesList.length;
     for(var i = 0; i < len; i++) {
         g_shapesList[i].render();
@@ -227,6 +210,16 @@ function renderScene(){
     box.matrix.scale(.3,.3,.3);
     box.matrix.translate(-.5, 0, -0.001);
     box.render();
+
+    // a bunch of rotating cubes
+    var K = 10.0;
+    for(var i = 1; i<K; i++){
+        var c = new Cube();
+        c.matrix.translate(-.8, 1.9*i/K-1.0, 0);
+        c.matrix.rotate(g_seconds*100,1,1,1);
+        c.matrix.scale(.1, .5/K, 1.0/K);
+        c.render();
+    }
 
     // check the time at the end of the function, and show on webpage
     var duration = performance.now() - startTime;
@@ -307,80 +300,3 @@ function connectVariablesToGLSL(){
     gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
 }
 
-
-// function to draw picture
-function drawPicture(){
-    g_pictureShapes = [];
-
-    // define colors
-    const hullColor = [0.373, 0.275, 0.180, 1.0];
-    const sailColor = [0.549, 0.533, 0.518, 1.0];
-    const waterColor = [0.220, 0.541, 0.757, 0.7];
-    const flagColor = [1.0, 0.102, 0.102, 1.0];
-    const mColor = [0.416, 0.482, 0.635, 1.0];
-    const wColor = [1.0, 0.875, 0.871, 1.0];
-
-    
-    createTriangle(-1.0, -1.0, 1.0, -1.0, -1.0, -0.4, waterColor);
-    createTriangle(1.0, -1.0, 1.0, -0.4, -1.0, -0.4, waterColor);
-    
-    createTriangle(-0.6, -0.4, 0.6, -0.4, 0.0, 0.1, hullColor); // big triangle
-    createTriangle(-0.5, -0.4, 0.5, -0.4, 0.5, -0.2, hullColor);
-    createTriangle(-0.5, -0.4, 0.5, -0.2, -0.5, -0.2, hullColor);
-    createTriangle(-0.4, -0.2, 0.4, -0.2, -0.4, 0.0, hullColor);
-    createTriangle(0.4, -0.2, 0.4, 0.0, -0.4, 0.0, hullColor);
-
-
-    createTriangle(-0.05, 0.0, 0.05, 0.0, -0.05, 0.6, sailColor);
-    createTriangle(0.05, 0.0, 0.05, 0.6, -0.05, 0.6, sailColor);
-    createTriangle(0.05, 0.2, 0.05, 0.5, 0.4, 0.35, flagColor);
-
-    // create initial M
-    // left |
-    createTriangle(0.0, 0.65, 0.03, 0.65, 0.0, 0.8, mColor);
-    createTriangle(0.03, 0.65, 0.03, 0.8, 0.0, 0.8, mColor);
-    // right |
-    createTriangle(0.08, 0.65, 0.11, 0.65, 0.08, 0.8, mColor);
-    createTriangle(0.11, 0.65, 0.11, 0.8, 0.08, 0.8, mColor);
-    // create a "v" shape for m
-    createTriangle(0.03, 0.8, 0.055, 0.72, 0.08, 0.8, mColor);
-
-    // create initial W
-    createTriangle(0.13, 0.65, 0.16, 0.65, 0.13, 0.8, wColor);
-    createTriangle(0.16, 0.65, 0.16, 0.8, 0.13, 0.8, wColor);
-    createTriangle(0.21, 0.65, 0.24, 0.65, 0.21, 0.8, wColor);
-    createTriangle(0.24, 0.65, 0.24, 0.8, 0.21, 0.8, wColor);
-    createTriangle(0.16, 0.67, 0.185, 0.8, 0.21, 0.67, wColor);
-
-    renderScene();
-}
-
-
-// a function that create triangle
-function createTriangle(x1, y1, x2, y2, x3, y3, color){
-    let triangle = new Triangle();
-    triangle.customVertices = [x1, y1, x2, y2, x3, y3];
-    triangle.position = [(x1+x2+x3)/3, (y1+ y2 + y3)/ 3];
-    triangle.color = color.slice();
-    triangle.size = 5.0;    // default triangle size
-    triangle.isPictureTriangle = true;
-
-    triangle.render = function() {
-        if (this.isPictureTriangle && this.customVertices) {
-            var rgba = this.color;
-            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-            drawTriangle(this.customVertices);
-        }else {
-            var xy = this.position;
-            var rgba = this.color;
-            var size = this.size;
-            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-            gl.uniform1f(u_Size, size);
-
-            var d = this.size/200.0;
-            drawTriangle([xy[0], xy[1], xy[0]+d, xy[1], xy[0], xy[1]+d]);
-        }
-    }
-    g_shapesList.push(triangle);
-    g_pictureShapes.push(triangle);
-}
