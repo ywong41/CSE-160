f57416d673d6d40812c036789af6d7fdcd75d719
root <root@DESKTOP-5IFO51V.localdomain>
2026-02-24 16:03:07 -0800

    add a sphere

--- asg4/src/Sphere.js
+++ asg4/src/Sphere.js
@@ -22,8 +22,8 @@ class Sphere {
         // Pass the matrix to u_ModelMatrix attribute
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-        var d = Math.PI / 10;
-        var dd = Math.PI / 100;
+        var d = Math.PI / 25;
+        var dd = Math.PI / 25;
 
         for (var t = 0; t < Math.PI; t += d) {
             for (var r = 0; r < (2 * Math.PI); r += d) {
@@ -32,21 +32,26 @@ class Sphere {
                 var p3 = [sin(t) * cos(r + dd), sin(t) * sin(r + dd), cos(t)];
                 var p4 = [sin(t + dd) * cos(r + dd), sin(t + dd) * sin(r + dd), cos(t + dd)];
 
+                var uv1 = [t / Math.PI, r / (2 * Math.PI)];
+                var uv2 = [(t + dd) / Math.PI, r / (2 * Math.PI)];
+                var uv3 = [t / Math.PI, (r + dd) / (2 * Math.PI)];
+                var uv4 = [(t + dd) / Math.PI, (r + dd) / (2 * Math.PI)];
+
                 var v = [];
                 var uv = [];
 
-                v = v.concat(p1); uv = uv.concat([0, 0]);
-                v = v.concat(p2); uv = uv.concat([0, 0]);
-                v = v.concat(p4); uv = uv.concat([0, 0]);
+                v = v.concat(p1); uv = uv.concat(uv1);
+                v = v.concat(p2); uv = uv.concat(uv2);
+                v = v.concat(p4); uv = uv.concat(uv4);
 
                 gl.uniform4f(u_FragColor, 1, 1, 1, 1);
                 drawTriangle3DUVNormal(v, uv, v);
 
                 v = []; uv = [];
 
-                v = v.concat(p1); uv = uv.concat([0, 0]);
-                v = v.concat(p4); uv = uv.concat([0, 0]);
-                v = v.concat(p3); uv = uv.concat([0, 0]);
+                v = v.concat(p1); uv = uv.concat(uv1);
+                v = v.concat(p4); uv = uv.concat(uv4);
+                v = v.concat(p3); uv = uv.concat(uv3);
 
                 gl.uniform4f(u_FragColor, 1, 0, 0, 1);
                 drawTriangle3DUVNormal(v, uv, v);
